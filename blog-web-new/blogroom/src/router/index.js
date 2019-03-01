9fdb902fdb5cb2bd51f2e6a8d16386786688d72d
qiuyunduo <qiuyunduo@m-chain.com>
2019-03-01 18:41:47 +0800

    开始重写博客屋前端

--- blog-web-new/blogroom/src/router/index.js
+++ blog-web-new/blogroom/src/router/index.js
@@ -1,15 +1,63 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
+import Layout from '@/view/layout/Layout'
+
+export const constantRouterMap = [
+  // {
+  //   path: '/redirect',
+  //   component: Layout,
+  //   hidden: true,
+  //   children: [
+  //     {
+  //       path: '/redirect/:path*',
+  //       component: () => import('@/views/redirect/index')
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: '/login',
+  //   component: () => import('@/views/login/index'),
+  //   hidden: true
+  // },
+  // {
+  //   path: '/auth-redirect',
+  //   component: () => import('@/views/login/authredirect'),
+  //   hidden: true
+  // },
+  // {
+  //   path: '/404',
+  //   component: () => import('@/views/errorPage/404'),
+  //   hidden: true
+  // },
+  // {
+  //   path: '/401',
+  //   component: () => import('@/views/errorPage/401'),
+  //   hidden: true
+  // },
+  {
+    path: '',
+    component: Layout
+  },
+  // {
+  //   path: '',
+  //   component: Layout,
+  //   redirect: 'dashboard',
+  //   children: [
+  //     {
+  //       path: 'dashboard',
+  //       component: () => import('@/views/dashboard/index'),
+  //       name: 'Dashboard',
+  //       meta: { title: 'dashboard', icon: 'dashboard', noCache: true }
+  //     }
+  //   ]
+  // }
+]
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: '我自己写的组件',
-      component: () => import('@/view/website/index')
-    }
-  ]
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap
 })
